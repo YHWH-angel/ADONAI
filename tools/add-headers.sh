c9db24cae7eb7b6b89c524775e5e94770edad019
YHWH-angel <94326903+YHWH-angel@users.noreply.github.com>
2025-08-30 18:14:49 +0200

    Remove unused tools directory

--- tools/add-headers.sh
+++
@@ -1,7 +0,0 @@
-YEAR=2025
-while IFS= read -r -d '' f; do
-  if grep -q 'The Bitcoin Core developers' "$f" && ! grep -q 'The Adonai Core developers' "$f"; then
-    sed -i "/The Bitcoin Core developers/a // Modifications (c) ${YEAR} The Adonai Core developers" "$f"
-    echo "[CHANGE] $f"
-  fi
-done < <(find src -type f \( -name "*.h" -o -name "*.cpp" \) -print0)
